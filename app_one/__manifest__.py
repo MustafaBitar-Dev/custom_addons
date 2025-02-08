79359da967a2798f59a3e155644cd9c7e418b031
Mustafa Bitar <mustafa.bitar.dev@gmail.com>
2025-02-08 11:10:26 +0300

    Create First Module
    Videos 0 - 6
    Important Information
    1- Manifest file used to define the module
    2- Init file used to import all python files in a directory  
    3- Model directory to store all models in the module

---
+++ app_one/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'App One',
+    'author': 'Mustafa Bitar',
+    'category': 'Test Apps',
+    'version': '17.0.0.1.0',
+    'depends': ['base'],
+    'description': "My first app",
+    'data': [],
+    'application': True
+}
